b2ff4e436550f113f8bc7e3f19ef0ccc79c6fc27
Ashraf Mia <ashmia@icps.local>
2025-10-04 13:05:24 +0600

    remove emoji file

--- src/lib/emoji.js
+++
@@ -1,9 +0,0 @@
-// Example: add emoji particle
-import { Particle } from './particles.js';
-
-export function addEmojiParticle(lib, x, y, emoji, options = {}) {
-    lib.particles.push(new Particle(x, y, {
-        ...options,
-        shape: emoji
-    }));
-}
